88abfb0025e409e1fd633aec3b6d2cf400dbd37c
BORBO <bwofodol@gmail.com>
2026-01-11 23:01:03 -0600

    add aegis

--- BadItemAcademy/BadItemAcademyPlugin.cs
+++ BadItemAcademy/BadItemAcademyPlugin.cs
@@ -26,8 +26,11 @@ using UnityEngine.ResourceManagement.AsyncOperations;
 namespace BadItemAcademy
 {
     [BepInDependency(R2API.LanguageAPI.PluginGUID, BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency(R2API.RecalculateStatsAPI.PluginGUID, BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency(R2API.ContentManagement.R2APIContentManager.PluginGUID, BepInDependency.DependencyFlags.HardDependency)]
 
     [NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+    [R2APISubmoduleDependency(nameof(ContentAddition))]
     [BepInPlugin(guid, modName, version)]
     public partial class BadItemAcademyPlugin : BaseUnityPlugin
     {
@@ -48,6 +51,15 @@ namespace BadItemAcademy
         internal static ConfigEntry<bool> ShouldBenthicWeighSelection { get; set; }
         internal static ConfigEntry<bool> InvertBenthicWeightedSelection { get; set; }
         internal static ConfigEntry<bool> BiasBenthicWeightedSelection { get; set; }
+        internal static ConfigEntry<float> AegisConversionInterval { get; set; }
+        internal static ConfigEntry<float> AegisConversionRate { get; set; }
+        internal static ConfigEntry<float> AegisRemovalRate { get; set; }
+        internal static ConfigEntry<float> AegisForceConversionThreshold { get; set; }
+        internal static ConfigEntry<int> AegisMaxFortificationStacks { get; set; }
+        internal static ConfigEntry<float> AegisMaxStatBonusBase { get; set; }
+        internal static ConfigEntry<float> AegisMaxStatBonusStack { get; set; }
+        internal static ConfigEntry<bool> AegisRevertHealingReduction { get; set; }
+        internal static ConfigEntry<bool> AegisUseFortification { get; set; }
 
 
         void Awake()
@@ -57,12 +69,84 @@ namespace BadItemAcademy
             RehabNkuhanas();
             RehabSingularityBand();
             RehabBenthic();
+            RehabAegis();
+
+            SaveConfig();
         }
+
         private static void DoConfig()
         {
             string section = "Bad Item Rehabilitation : ";
 
             CustomConfigFile = new ConfigFile(Paths.ConfigPath + $"\\{modName}.cfg", true);
+            CustomConfigFile.SaveOnConfigSet = false;
+            #region aegis
+            AegisConversionInterval = CustomConfigFile.Bind(
+                section + "Aegis",
+                "Aegis Conversion to Fortification Interval",
+                _AegisConversionInterval,
+                "Determines the rate at which cumulated barrier decay is converted to Fortification. " +
+                    "By default, this is made to be a fairly high value in order to reduce the performance cost of changing stats frequently. " +
+                    "However, the Force Conversion Threshold makes sure that Fortification is not added too slowly for the changing stats."
+                );
+            AegisConversionRate = CustomConfigFile.Bind(
+                section + "Aegis",
+                "Aegis Rate Of Conversion To Fortification",
+                _AegisConversionRate,
+                "Determines the multiplier of barrier converted to Fortification. " +
+                    "For example, with a value of 0.5, 100% barrier decay only gives 50% Fortification. "
+                );
+            AegisRemovalRate = CustomConfigFile.Bind(
+                section + "Aegis",
+                "Aegis Rate Of Conversion To Fortification",
+                _AegisRemovalRate,
+                "Determines the multiplier of Fortification removal when taking damage, proportional to maximum health. " +
+                    "For example, with a value of 2, losing 50% of your max health will take away 100% Fortification. "
+                );
+            AegisForceConversionThreshold = CustomConfigFile.Bind(
+                section + "Aegis",
+                "Aegis Force Conversion To Fortification Threshold",
+                _AegisForceConversionThreshold,
+                "Determines the threshold at which cumulated barrier decay is forced to be converted to Fortification, " +
+                    "bypassing the cooldown and adding all cumulated barrier to Fortification instantly. " +
+                    "This is shown in the item's full description as the minimum value to convert to Fortification. "
+                );
+            AegisMaxFortificationStacks = CustomConfigFile.Bind(
+                section + "Aegis",
+                "Aegis Max Fortification Stacks",
+                _AegisMaxFortificationStacks,
+                "Determines the maximum stacks of Fortification required for the full bonus. "
+                );
+            AegisMaxStatBonusBase = CustomConfigFile.Bind(
+                section + "Aegis",
+                "Aegis Fortification Max Stat Bonus BASE",
+                _AegisMaxStatBonusBase,
+                "Determines the amount Aegis increases ALL STATS with full Fortification with your first stack of Aegis. " +
+                    "Scales linearly, represented as a percent. "
+                );
+            AegisMaxStatBonusStack = CustomConfigFile.Bind(
+                section + "Aegis",
+                "Aegis Fortification Max Stat Bonus PER STACK",
+                _AegisMaxStatBonusStack,
+                "Determines the amount Aegis increases ALL STATS with full Fortification with additional stacks of Aegis. " +
+                    "Scales linearly, represented as a percent. "
+                );
+            AegisRevertHealingReduction = CustomConfigFile.Bind(
+                section + "Aegis",
+                "Should Aegis Revert Healing Reductions When Converting To Barrier?",
+                _AegisRevertHealingReduction,
+                "Vanilla is FALSE. If set to TRUE, Aegis will revert any healing cuts made by modifiers like Eclipse 5 when " +
+                    "applying overheal to barrier."
+                );
+            AegisUseFortification = CustomConfigFile.Bind(
+                section + "Aegis",
+                "Should Aegis Use Fortification?",
+                _AegisUseFortification,
+                "Vanilla is FALSE. If set to TRUE, Aegis will turn all decayed barrier into a stat buff called Fortification. " +
+                    "Fortification boosts ALL stats, but is reduced when taking damage by an amount proportional to your max health."
+                );
+            #endregion
+            #region benthic
             ShouldBenthicWeighSelection = CustomConfigFile.Bind(
                 section + "Benthic Bloom",
                 "Should Benthic Bloom Weigh Selection?",
@@ -89,6 +173,8 @@ namespace BadItemAcademy
                     "adjust its weighted selection to account for the size of your inventory or allow it to choose whatever it wants. " +
                     "Neither of these options resemble vanilla behavior, so do what you want!"
                 );
+            #endregion
+            #region nkuhanas
             PoolHealingBeforeModifiers = CustomConfigFile.Bind(
                 section + "NKuhanas Opinion",
                 "Pool Healing Before Modifiers (Affects Corpsebloom)",
@@ -132,6 +218,7 @@ namespace BadItemAcademy
                 "Vanilla is 40. Determines the maximum range of skulls fired " +
                     "by healing with NKuhanas Opinion. Represented in meters."
                 );
+            #endregion
             #region singularity band
             VoidBandDamageMult = CustomConfigFile.Bind(
                 section + "Singularity Band",
@@ -148,6 +235,12 @@ namespace BadItemAcademy
                     "created by Singularity Band."
                 );
             #endregion
+        }
+
+        private static void SaveConfig()
+        {
+            CustomConfigFile.SaveOnConfigSet = true;
+            CustomConfigFile.Save();
         }
 
         public static AssetReferenceT<T> LoadAsync<T>(string guid, Action<T> callback) where T : UnityEngine.Object
---
+++ BadItemAcademy/Items/Aegis.cs
@@ -0,0 +1,283 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using R2API;
+using RoR2;
+using RoR2.Items;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Networking;
+using static R2API.RecalculateStatsAPI;
+[assembly: HG.Reflection.SearchableAttribute.OptIn]
+
+namespace BadItemAcademy
+{
+    public partial class BadItemAcademyPlugin
+    {
+
+        public static BuffDef AegisFortificationBuff;
+
+        private static float _AegisConversionInterval = 1f;
+        private static float _AegisConversionRate = 1f;
+        private static float _AegisRemovalRate = 1f;
+        private static float _AegisForceConversionThreshold = 0.05f;
+        private static int _AegisMaxFortificationStacks = 100;
+        private static float _AegisMaxStatBonusBase = 0.15f;
+        private static float _AegisMaxStatBonusStack = 0.1f;
+        private static bool _AegisRevertHealingReduction = true;
+        private static bool _AegisUseFortification = true;
+
+        private static float incomingHealingCache = 0;
+        private static float modifiedHealingCache = 0;
+        private static float barrierDecayedCache = 0;
+        public static void RehabAegis()
+        {
+            AegisFortificationBuff = ScriptableObject.CreateInstance<BuffDef>();
+            AegisFortificationBuff.name = "bdBarrierFortification";
+            AegisFortificationBuff.iconSprite = Addressables.LoadAssetAsync<Sprite>("RoR2/Base/Common/texBuffGenericShield.tif").WaitForCompletion();
+            AegisFortificationBuff.buffColor = Color.yellow;
+            AegisFortificationBuff.canStack = true;
+            AegisFortificationBuff.isDebuff = false;
+            AegisFortificationBuff.stackingDisplayMethod = BuffDef.StackingDisplayMethod.Percentage;
+
+            ContentAddition.AddBuffDef(AegisFortificationBuff);
+
+            GetStatCoefficients += AegisStatCoefficients;
+
+            LanguageAPI.Add("ITEM_BARRIERONOVERHEAL_PICKUP", "Healing past full grants you a temporary barrier. Decayed barrier boosts ALL stats.");
+            LanguageAPI.Add("ITEM_BARRIERONOVERHEAL_DESC", 
+                $"Healing past full grants you a <style=cIsHealing>temporary barrier</style> " +
+                $"for <style=cIsHealing>50% <style=cStack>(+50% per stack)</style></style> " +
+                $"of the amount you <style=cIsHealing>healed</style>. " +
+                (AegisUseFortification.Value == false ? "" :
+                $"Every <style=cIsHealing>{AegisForceConversionThreshold.Value * 100}%</style> barrier that decays is converted into " +
+                $"<style=cIsDamage>Fortification</style>, increasing <style=cIsDamage>ALL</style> stats " +
+                $"by up to <style=cIsDamage>{AegisMaxStatBonusBase.Value * 100}%</style> " +
+                $"<style=cStack>(+{AegisMaxStatBonusStack.Value * 100}% per stack)</style>. " +
+                $"<style=cIsDamage>Fortification</style> is lost when taking damage."));
+
+            IL.RoR2.HealthComponent.ServerFixedUpdate += HealthComponent_ServerFixedUpdate_CumulateBarrierDecay;
+            IL.RoR2.HealthComponent.Heal += HealthComponent_Heal_OverhealToBarrier;
+        }
+
+
+        private static void AegisStatCoefficients(CharacterBody sender, StatHookEventArgs args)
+        {
+            if (AegisUseFortification.Value == false)
+                return;
+            int itemCount = sender.inventory?.GetItemCountEffective(RoR2Content.Items.BarrierOnOverHeal) ?? 0;
+            int buffCount = sender.GetBuffCount(AegisFortificationBuff);
+            if(buffCount > 0 && itemCount > 0)
+            {
+                float aegisMaxStatBonus = AegisMaxStatBonusBase.Value + AegisMaxStatBonusStack.Value * (itemCount - 1);
+                float statBonus = aegisMaxStatBonus * ((float)buffCount / (float)AegisMaxFortificationStacks.Value);
+                args.healthMultAdd += statBonus;
+                args.regenMultAdd += statBonus;
+                args.moveSpeedMultAdd += statBonus;
+                args.damageMultAdd += statBonus;
+                args.attackSpeedMultAdd += statBonus;
+                args.critAdd += statBonus * 100;
+                args.armorTotalMult += statBonus;
+            }
+        }
+
+        private static void HealthComponent_Heal_OverhealToBarrier(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            int modifiedHealingLoc = 2;
+
+            //before any changes are made to healing, store the in value. this wont do anything if the other matches/hooks fail
+            bool b1 = c.TryGotoNext(MoveType.After,
+                x => x.MatchLdfld<HealthComponent.ItemCounts>(nameof(HealthComponent.ItemCounts.increaseHealing))
+                )
+                && c.TryGotoNext(MoveType.After,
+                x => x.MatchStarg(1)
+                );
+            if (!b1)
+            {
+                DebugBreakpoint(nameof(HealthComponent_Heal_OverhealToBarrier), 1);
+                c.Index = 0;
+            }
+            else
+            {
+                c.Index++;
+            }
+            c.Emit(OpCodes.Ldarg_1);
+            c.EmitDelegate<Action<float>>((incomingHealing) =>
+            {
+                incomingHealingCache = incomingHealing;
+            });
+
+            //after all modifiers are made to healing, store the modified value before it gets reduced. this wont do anything if the other matches/hooks fail
+            bool b2 = c.TryGotoNext(MoveType.After,
+                x => x.MatchLdarg(1),
+                x => x.MatchStloc(out modifiedHealingLoc)
+                );
+            if (!b2)
+            {
+                DebugBreakpoint(nameof(HealthComponent_Heal_OverhealToBarrier), 2);
+                return;
+            }
+            c.Emit(OpCodes.Ldloc, modifiedHealingLoc);
+            c.EmitDelegate<Action<float>>((modifiedHealing) =>
+            {
+                modifiedHealingCache = modifiedHealing;
+            });
+
+            //where healing gets added to barrier, we reverse . this is the only match/hook that does anything
+            bool b3 = c.TryGotoNext(MoveType.Before,
+                x => x.MatchCallOrCallvirt<HealthComponent>(nameof(HealthComponent.AddBarrier))
+                )
+                && c.TryGotoPrev(MoveType.After, 
+                x => x.MatchLdloc(modifiedHealingLoc)
+                );
+            if (!b3)
+            {
+                DebugBreakpoint(nameof(HealthComponent_Heal_OverhealToBarrier), 3);
+                return;
+            }
+
+            c.EmitDelegate<Func<float, float>>((healingRemainder) =>
+            {
+                if (healingRemainder == 0 || modifiedHealingCache == 0 || AegisRevertHealingReduction.Value == false)
+                    return healingRemainder;
+
+                //if healing wasnt cut into health, skip the math
+                if (healingRemainder == modifiedHealingCache)
+                    return incomingHealingCache;
+
+                float healingCompFactor = incomingHealingCache / modifiedHealingCache;
+                if (healingCompFactor <= 1)
+                    return healingRemainder;
+
+                return healingRemainder * healingCompFactor;
+            });
+        }
+
+        private static void HealthComponent_ServerFixedUpdate_CumulateBarrierDecay(ILContext il)
+        {
+            ILCursor c = new ILCursor(il);
+
+            bool b1 = c.TryGotoNext(MoveType.Before,
+                x => x.MatchCallOrCallvirt<HealthComponent>("set_Networkbarrier")
+                );
+            if (!b1)
+            {
+                DebugBreakpoint(nameof(HealthComponent_ServerFixedUpdate_CumulateBarrierDecay), 1);
+                return;
+            }
+
+            c.Emit(OpCodes.Ldarg_0);
+            c.EmitDelegate<Func<float, HealthComponent, float>>((newBarrier, self) =>
+            {
+                barrierDecayedCache = self.barrier - newBarrier;
+                return newBarrier;
+            });
+
+            c.Index++;
+            c.Emit(OpCodes.Ldarg_0);
+            c.EmitDelegate<Action<HealthComponent>>((self) =>
+            {
+                if (self.itemCounts.barrierOnOverHeal > 0 && NetworkServer.active && AegisUseFortification.Value == true)
+                {
+                    AegisItemBehavior aegisItemBehavior = self.body.GetComponent<AegisItemBehavior>();
+                    if (aegisItemBehavior != null && aegisItemBehavior.stack > 0)
+                        aegisItemBehavior.OnBarrierDecayed(barrierDecayedCache);
+                }
+                //Action<HealthComponent, float> action2 = onHealthComponentBarrierDecayedGlobal;
+                //if (action2 != null)
+                //{
+                //    action2(self, self.barrier - newBarrier);
+                //}
+            });
+        }
+    }
+
+    public class AegisItemBehavior : BaseItemBodyBehavior, IOnTakeDamageServerReceiver
+    {
+        public float decayedBarrierCumulative = 0;
+        private float aegisConversionStopwatch = 0;
+        private bool isAtFullFortification = false;
+
+        [ItemDefAssociation(useOnServer = true, useOnClient = false)]
+        private static ItemDef GetItemDef() => RoR2Content.Items.BarrierOnOverHeal;
+        private static BuffDef GetBuffDef() => BadItemAcademyPlugin.AegisFortificationBuff;
+        public HealthComponent healthComponent => this.body.healthComponent;
+
+        void Start()
+        {
+            if(BadItemAcademyPlugin.AegisUseFortification.Value == true)
+                body?.healthComponent?.AddOnTakeDamageServerReceiver(this);
+        }
+        void OnDestroy()
+        {
+            body?.healthComponent?.RemoveOnTakeDamageServerReceiver(this);
+            this.body.SetBuffCount(GetBuffDef().buffIndex, 0);
+        }
+
+        public void CumulateBarrierDecay(HealthComponent healthComponent, float barrierDecayed)
+        {
+            if (healthComponent == this.healthComponent)
+            {
+                OnBarrierDecayed(barrierDecayed);
+            }
+        }
+
+        public void OnBarrierDecayed(float barrierDecayed)
+        {
+            if (isAtFullFortification)
+                return;
+            decayedBarrierCumulative += barrierDecayed * BadItemAcademyPlugin.AegisConversionRate.Value;
+
+            aegisConversionStopwatch += Time.fixedDeltaTime;
+            if (this.healthComponent.barrier <= float.Epsilon 
+                || decayedBarrierCumulative >= healthComponent.fullCombinedHealth * BadItemAcademyPlugin.AegisForceConversionThreshold.Value
+                || aegisConversionStopwatch >= BadItemAcademyPlugin.AegisConversionInterval.Value)
+            {
+                aegisConversionStopwatch = 0;
+                int buffStacks = 0;
+                if(this.healthComponent.barrier <= float.Epsilon)
+                {
+                    buffStacks = Mathf.CeilToInt((100 * decayedBarrierCumulative) / healthComponent.fullCombinedHealth);
+                    decayedBarrierCumulative = 0;
+                }
+                else
+                {
+                    float healthPerBuff = healthComponent.fullCombinedHealth / 100;
+                    buffStacks = Mathf.FloorToInt(decayedBarrierCumulative / healthPerBuff);
+                    decayedBarrierCumulative = decayedBarrierCumulative - (healthPerBuff * buffStacks);
+                }
+
+                int currentBuffCount = this.body.GetBuffCount(GetBuffDef().buffIndex);
+                int newBuffCount = currentBuffCount + buffStacks;
+                if (newBuffCount >= BadItemAcademyPlugin.AegisMaxFortificationStacks.Value)
+                {
+                    newBuffCount = BadItemAcademyPlugin.AegisMaxFortificationStacks.Value;
+                    isAtFullFortification = true;
+                }
+                this.body.SetBuffCount(GetBuffDef().buffIndex, newBuffCount);
+            }
+        }
+
+        public void OnTakeDamageServer(DamageReport damageReport)
+        {
+            HealthComponent victimHealthComponent = damageReport.victimBody?.healthComponent;
+            if (damageReport.victimBody?.healthComponent == null)
+                return;
+
+            int currentBuffCount = this.body?.GetBuffCount(GetBuffDef().buffIndex) ?? 0;
+            if (currentBuffCount == 0)
+                return;
+
+            float healthLost = damageReport.combinedHealthBeforeDamage - victimHealthComponent.combinedHealth;
+            int buffStacksToLose = Mathf.FloorToInt((100 * healthLost * BadItemAcademyPlugin.AegisRemovalRate.Value) / victimHealthComponent.fullCombinedHealth);
+
+            damageReport.victimBody.SetBuffCount(GetBuffDef().buffIndex, Mathf.Max(currentBuffCount - buffStacksToLose, 0));
+
+            isAtFullFortification = false;
+        }
+    }
+}
